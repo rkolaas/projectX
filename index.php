8c39ee8a60638338f67fd12434e03e927135334c
Robert Kolaas <me@robertkolaas.no>
2018-02-08 20:53:16 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>robertkolaas.com</title>
+</head>
+<body>
+	<div class="wrapper">
+		<div class="header">
+		</div>
+		<div class="nav">
+		</div>
+		<div class="container">
+		</div>
+		<div class="footer">
+		</div>
+	</div>
+</body>
+</html>
