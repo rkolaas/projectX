1c5d478efbdfd7daf7e887a0430a17679e71c7a4
Robert Kolaas <me@robertkolaas.no>
2018-02-09 14:17:12 +0100

    updated skeleton and set up the grid sections, also colorized

--- index.php
+++ index.php
@@ -3,22 +3,27 @@
 <head>
 	<link rel="stylesheet" type="text/css" href="assets/css/styles.css">
 	<title>robertkolaas.com</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 </head>
 <body>
 	<div class="wrapper">
 		<div class="nav">
-			<a class="home">HJEM</a>
-			<a class="cv">CV</a>
-			<a class="contact">KONTAKT</a>
+			<p class="nav-links">
+				<a href="#">HJEM</a> //
+				<a href="#cv">CV</a> //
+				<a href="#contact">KONTAKT</a>
+			</p>
 		</div>
 		<div class="header">
 		</div>
-		
-			
+		<div class="cv">
+			<a name="cv"><h2>CV</h2></a>
 		</div>
-		<div class="main">
+		<div class="contact">
+			<a  a name="contact"><h2>Kontakt</h2></a>
 		</div>
 		<div class="footer">
+			<p id="footnote">Copyright &copy; <a href="#contact">Robert Kolås</a></p>
 		</div>
 	</div>
 </body>
