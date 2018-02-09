621c1190bd206a38313f81fb026772d180115b0f
Robert Kolaas <me@robertkolaas.no>
2018-02-09 10:42:37 +0100

    fixed problem with css-grid

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="stylesheet" type="text/css" href="styles.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/styles.css">
 	<title>robertkolaas.com</title>
 </head>
 <body>
