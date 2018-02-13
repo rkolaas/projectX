e0ef2e81fd83c05f7f6ab6607550ad6ac08a3001
Robert Kolaas <me@robertkolaas.no>
2018-02-13 14:24:22 +0100

    starting fresh

--- index.php
+++ index.php
@@ -9,20 +9,14 @@
 	<div class="wrapper">
 		<div class="nav">
 			<p class="nav-links">
-				<a href="#">HJEM</a> //
+				<a href="#">PORTIFOLIO</a> //
 				<a href="#cv">CV</a> //
 				<a href="#contact">KONTAKT</a>
 			</p>
-			<img class="logo" src="assets/gfx/logo.png">
 		</div>
 		<div class="header">
 		</div>
-		<div class="cv">
-			<a name="cv"><h2>CV</h2></a>
-		</div>
-		<div class="contact">
-			<a  a name="contact"><h2>Kontakt</h2></a>
-		</div>
+		
 		<div class="footer">
 			<p id="footnote">Copyright &copy; <a href="#contact">Robert Kolås</a></p>
 		</div>
