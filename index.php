3e1351b6332a6e302ad0d7b7845ba2998f2d3b99
Robert Kolaas <me@robertkolaas.no>
2018-04-11 22:30:12 +0200

    started on layput
    testing push
    
    still testing

--- index.php
+++ index.php
@@ -1,14 +1,39 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
         <title>robertkolaas</title>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">    
-        <!-- Latest compiled and minified JavaScript -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
         <link rel="stylesheet" type="text/css" href="/assets/css/stylesheet.css">
     </head>
     <body>
+        <div class="col-sm-12" id="nav" role="navigation">
+                <div class="col-sm-3" id="title">
+                    <h1>testing testing testing</h1>
+                </div>
+                <div class="col-sm-6" id="nav2">
+                    <h1>testing testing</h1>
+                </div>
+                <div class="col-sm-3" id="links">
+                    <h1>testing testing</h1>
+                </div>
+        </div>
+        <div class="container-fluid" id="container">
+            <div class="row" id="header">
+                <div class="col-sm-3">
+                </div>
+                <div class="col-sm-6" id="content">
+                    <h1>TEST TEST</h1>
+                </div>
+                <div class="col-sm-3">
+                </div>
+            </div>
+        </div>
+
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
     </body>
 </html>
