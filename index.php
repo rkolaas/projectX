6ff6782081d171f531eda6ae76e983a355c171a0
Robert Kolaas <me@robertkolaas.no>
2018-04-12 11:55:35 +0200

    updated layout

--- index.php
+++ index.php
@@ -3,25 +3,37 @@
     <head>
         <title>robertkolaas</title>
         <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">    
         <link rel="stylesheet" type="text/css" href="/assets/css/stylesheet.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <!-- Includes -->
+        <script> 
+            $(function(){
+                $("#links").load("/assets/includes/nav.html");
+                $("#copyright").load("/assets/includes/copyright.php");
+                $("#socialLinks").load("/assets/includes/socialLinks.html");
+            });
+        </script> 
     </head>
     <body>
-        <div class="col-sm-12" id="nav" role="navigation">
+        <!-- Navigation bar -->
+        <div class="col-sm-12 navbar-fixed-top" id="nav" role="navigation" >
                 <div class="col-sm-3" id="title">
-                    <h1>testing testing testing</h1>
+                    <h2>robertkolaas</h2>
                 </div>
                 <div class="col-sm-6" id="nav2">
-                    <h1>testing testing</h1>
                 </div>
-                <div class="col-sm-3" id="links">
-                    <h1>testing testing</h1>
+                <div class="col-sm-3" id="links">  
+                    <!-- imported nav.html -->   
                 </div>
         </div>
-        <div class="container-fluid" id="container">
-            <div class="row" id="header">
+        <!-- sections -->
+        <div class="container-fluid" id="section">
+            <div class="row section" id="section1">
                 <div class="col-sm-3">
                 </div>
                 <div class="col-sm-6" id="content">
@@ -30,10 +42,31 @@
                 <div class="col-sm-3">
                 </div>
             </div>
+            <!-- Section 2 -->
+            <div class="row section" id="section2">
+                <div class="col-sm-3">
+                </div>
+                <div class="col-sm-6" id="content">
+                    <h1>TEST TEST</h1>
+                </div>
+                <div class="col-sm-3">
+                </div>
+            </div>
+            <!-- footer -->
+            <div class="row footer" id="footer">
+                <div class="col-sm-3" id="socialLinks">  
+                    <!-- imported socialLinks.html -->   
+                </div>
+                <div class="col-sm-6" id="copyright">  
+                    <!-- imported copyright.php -->             
+                </div>
+                <div class="col-sm-3">
+                    <p>kontakt meg plz</p>
+                </div>
+            </div>
         </div>
 
 
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
+        
     </body>
 </html>
