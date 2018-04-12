07ace82703fb6a933e2768969bd136fb3a0ee58e
Robert Kolaas <me@robertkolaas.no>
2018-04-12 15:08:36 +0200

    updated layout

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" type="text/css" href="/assets/css/stylesheet.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <!-- Includes -->
@@ -16,6 +17,7 @@
                 $("#links").load("/assets/includes/nav.html");
                 $("#copyright").load("/assets/includes/copyright.php");
                 $("#socialLinks").load("/assets/includes/socialLinks.html");
+                $("#contact").load("/assets/includes/contact.html");
             });
         </script> 
     </head>
@@ -34,22 +36,22 @@
         <!-- sections -->
         <div class="container-fluid" id="section">
             <div class="row section" id="section1">
-                <div class="col-sm-3">
+                <div class="col-sm col-md-2 col-lg-2">
                 </div>
-                <div class="col-sm-6" id="content">
-                    <h1>TEST TEST</h1>
+                <div class="col-sm-12 col-md-8 col-lg-8" id="content">
+                    <h1>Section 1</h1>
                 </div>
-                <div class="col-sm-3">
+                <div class="col-sm col-md-2 col-lg-2">
                 </div>
             </div>
             <!-- Section 2 -->
             <div class="row section" id="section2">
-                <div class="col-sm-3">
+                <div class="col-sm col-md-2 col-lg-2">
                 </div>
-                <div class="col-sm-6" id="content">
-                    <h1>TEST TEST</h1>
+                <div class="col-sm-12 col-md-8 col-lg-8" id="content">
+                    <h1>Section 2</h1>
                 </div>
-                <div class="col-sm-3">
+                <div class="col-sm col-md-2 col-lg-2">
                 </div>
             </div>
             <!-- footer -->
@@ -60,8 +62,7 @@
                 <div class="col-sm-6" id="copyright">  
                     <!-- imported copyright.php -->             
                 </div>
-                <div class="col-sm-3">
-                    <p>kontakt meg plz</p>
+                <div class="col-sm-3" id="contact">
                 </div>
             </div>
         </div>
